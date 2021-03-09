4fa31a530a7370b8cd29d447af7805f1183d1453
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-09 19:12:43 -0300

    'module 1 - using typescript': added typescript

--- aulas/reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ aulas/reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,14 +3,22 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
       .then(response => response.json())
       .then(data => setRepositories(data)) 
   }, []);
+
+  console.log('repositories', repositories);
 
   return (
     <section className="repository-list">
@@ -19,7 +27,7 @@ export function RepositoryList() {
       <ul>
 
         {repositories.map((repository) => (
-          <RepositoryItem repository={repository} key={repository.id} />
+          <RepositoryItem repository={repository} key={repository.name} />
         ))}
       </ul>
     </section>
