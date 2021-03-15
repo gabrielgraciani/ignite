939850a2a5c89260aa577f74a221012d9425ddd5
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-15 19:40:11 -0300

    module2 - finish componentalizing

--- aulas/reactjs/02-dtmoney/src/App.tsx
+++ aulas/reactjs/02-dtmoney/src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
 import {GlobalStyle} from './styles/global'
 
 export function App() {
   return (
-    <div className="App">
-      <div className="title">teste</div>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
