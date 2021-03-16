9ac6486197056293b5e6fa565f0260fa6071acb1
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-16 20:30:37 -0300

    module3 - finish modal & forms

--- aulas/reactjs/02-dtmoney/src/App.tsx
+++ aulas/reactjs/02-dtmoney/src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import {GlobalStyle} from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal'
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransationModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransationModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransationModal} />
       <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransationModal} />
+
       <GlobalStyle />
     </>
   );
