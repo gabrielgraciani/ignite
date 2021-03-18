0a7591245bf1cc2ce95ae2f33cafa60c876c1180
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-18 18:14:54 -0300

    finish second challenge - componentalizing application

---
+++ desafios/reactjs/02-componentalizando-aplicacao/src/components/Content.tsx
@@ -0,0 +1,54 @@
+
+import { useState, useEffect } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
+}
+
+export function Content({ selectedGenre, selectedGenreId }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+  </div>
+  )
+}
