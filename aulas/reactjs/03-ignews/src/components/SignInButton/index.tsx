b125f9f492fc49ecc4118ad296bd0d2a25dae2c2
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-23 18:52:21 -0300

    module3 - added stripe payment method, faunadb to use with nextjs api and github oauth authentication

--- aulas/reactjs/03-ignews/src/components/SignInButton/index.tsx
+++ aulas/reactjs/03-ignews/src/components/SignInButton/index.tsx
@@ -1,17 +1,20 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04b361" />
-      Gabriel Graciani
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
