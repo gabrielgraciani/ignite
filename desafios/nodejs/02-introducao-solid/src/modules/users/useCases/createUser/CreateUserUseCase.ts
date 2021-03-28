1725ed1ca8c063b2112eec1ea42f70453ed062d7
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-28 08:46:38 -0300

    finish challenge node

---
+++ desafios/nodejs/02-introducao-solid/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    name: string;
+    email: string;
+}
+
+class CreateUserUseCase {
+    constructor(private usersRepository: IUsersRepository) {}
+
+    execute({ email, name }: IRequest): User {
+        const user = this.usersRepository.findByEmail(email);
+        if (user) {
+            throw new Error("Email já cadastrado");
+        }
+        return this.usersRepository.create({
+            email,
+            name,
+        });
+    }
+}
+
+export { CreateUserUseCase };
