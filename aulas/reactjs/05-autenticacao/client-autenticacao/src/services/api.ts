98ce79cff7b8a6a66165f7c68703ba93a8252eba
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-04 20:18:26 -0300

    added request queue with axios

--- aulas/reactjs/05-autenticacao/client-autenticacao/src/services/api.ts
+++ aulas/reactjs/05-autenticacao/client-autenticacao/src/services/api.ts
@@ -2,6 +2,8 @@ import { parseCookies, setCookie } from 'nookies';
 import axios, { AxiosError } from 'axios';
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -21,33 +23,60 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { 'igniteNextauth.refreshToken': refreshToken } = cookies;
+        const originalConfig = error.config;
 
-        api
-          .post('/refresh', {
-            refreshToken,
-          })
-          .then(response => {
-            const { token } = response.data;
+        if (!isRefreshing) {
+          isRefreshing = true;
 
-            setCookie(undefined, 'igniteNextauth.token', token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: '/',
-            });
-            setCookie(
-              undefined,
-              'igniteNextauth.refreshToken',
-              response.data.refreshToken,
-              {
+          api
+            .post('/refresh', {
+              refreshToken,
+            })
+            .then(response => {
+              const { token } = response.data;
+
+              setCookie(undefined, 'igniteNextauth.token', token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: '/',
-              },
-            );
+              });
+              setCookie(
+                undefined,
+                'igniteNextauth.refreshToken',
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                },
+              );
 
-            api.defaults.headers.Authorization = `Bearer ${token}`;
+              api.defaults.headers.Authorization = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach(request => request.onSuccess(token));
+              failedRequestsQueue = [];
+            })
+            .catch(err => {
+              failedRequestsQueue.forEach(request => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers.Authorization = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
-      } else {
-        // deslogar o usuario
+        });
       }
+      // deslogar o usuario
     }
   },
 );
