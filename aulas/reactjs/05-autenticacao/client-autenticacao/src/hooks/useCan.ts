619a41ecc91fe65b7a14a7ed66f3aeaa77463325
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-08 09:12:31 -0300

    validating permissions using server

--- aulas/reactjs/05-autenticacao/client-autenticacao/src/hooks/useCan.ts
+++ aulas/reactjs/05-autenticacao/client-autenticacao/src/hooks/useCan.ts
@@ -1,4 +1,5 @@
 import { useAuth } from '../contexts/AuthContext';
+import { validateUserPermissions } from '../utils/validateUserPermissions';
 
 type UseCanParams = {
   permissions?: string[];
@@ -10,21 +11,11 @@ export function useCan({ permissions, roles }: UseCanParams): boolean {
 
   if (!isAuthenticated) return false;
 
-  if (permissions?.length > 0) {
-    const hasAllPermissions = permissions.every(permission => {
-      return user.permissions.includes(permission);
-    });
+  const userHasValidPermissions = validateUserPermissions({
+    user,
+    permissions,
+    roles,
+  });
 
-    if (!hasAllPermissions) return false;
-  }
-
-  if (roles?.length > 0) {
-    const hasAllRoles = roles.some(role => {
-      return user.roles.includes(role);
-    });
-
-    if (!hasAllRoles) return false;
-  }
-
-  return true;
+  return userHasValidPermissions;
 }
--- aulas/reactjs/05-autenticacao/client-autenticacao/src/utils/withSSRAuth.ts
+++ aulas/reactjs/05-autenticacao/client-autenticacao/src/utils/withSSRAuth.ts
@@ -4,21 +4,53 @@ import {
   GetServerSidePropsResult,
 } from 'next';
 import { parseCookies, destroyCookie } from 'nookies';
-import { AuthTokenError } from '../errors/AuthTokenError';
+import decode from 'jwt-decode';
 
-export function withSSRAuth<T>(fn: GetServerSideProps<T>) {
+import { AuthTokenError } from '../errors/AuthTokenError';
+import { validateUserPermissions } from './validateUserPermissions';
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+};
+
+export function withSSRAuth<T>(
+  fn: GetServerSideProps<T>,
+  options?: WithSSRAuthOptions,
+) {
   return async (
     ctx: GetServerSidePropsContext,
   ): Promise<GetServerSidePropsResult<T>> => {
     const cookies = parseCookies(ctx);
+    const token = cookies['igniteNextauth.token'];
 
-    if (!cookies['igniteNextauth.token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
           permanent: false,
         },
       };
+    }
+
+    if (options) {
+      const user = decode<{ permissions: string[]; roles: string[] }>(token);
+      const { permissions, roles } = options;
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      });
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          },
+        };
+      }
     }
 
     try {
