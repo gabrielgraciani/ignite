41134701e68171dc0d86de91ca9565ff303ff937
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-08 12:36:39 -0300

    view about useMemo

--- aulas/reactjs/06-performance-react/README.md
+++ aulas/reactjs/06-performance-react/README.md
@@ -1,2 +1,9 @@
-Quando utilizar Memo
+When use Memo
 - 1. Pure Functional Components
+- 2. Renders too often
+- 3. Re-renders with same props
+- 4. Medium to big size
+
+When use useMemo
+- 1. Cálculos pesados
+- 2. Igualdade referencial (quando a gente repassa aquela informação a um compoennte filho)
--- aulas/reactjs/06-performance-react/src/components/SearchResults.tsx
+++ aulas/reactjs/06-performance-react/src/components/SearchResults.tsx
@@ -1,4 +1,6 @@
-import { ProductItem } from "./ProductItem"
+import { useMemo } from 'react';
+
+import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
   results: Array<{
@@ -9,8 +11,15 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem product={product} />
