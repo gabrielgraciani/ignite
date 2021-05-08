fc88274213fe552298c3e027f814ea295a9aa79c
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-08 09:02:31 -0300

    created hook and component permission

--- aulas/reactjs/05-autenticacao/client-autenticacao/src/pages/dashboard.tsx
+++ aulas/reactjs/05-autenticacao/client-autenticacao/src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Can } from '../components/Can';
 import { useAuth } from '../contexts/AuthContext';
 import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
@@ -13,7 +14,15 @@ export default function Dashboard(): JSX.Element {
       .then(response => console.log(response))
       .catch(err => console.log(err));
   }, []);
-  return <h1>dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async ctx => {
