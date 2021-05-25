34370a1b911c846bd9e5cd7be190aa8bde32e851
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-25 18:55:17 -0300

    creating go finances

---
+++ aulas/react-native/modulo2/gofinances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/42574064?v=4' }} />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
+}
