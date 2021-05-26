235e7130ebb32b8703eb5b01d53cd6916796d678
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-26 19:54:39 -0300

    finish transactions component

--- aulas/react-native/modulo2/gofinances/src/screens/Dashboard/index.tsx
+++ aulas/react-native/modulo2/gofinances/src/screens/Dashboard/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
-import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon, HighlightCards } from './styles';
+import { 
+  Container, 
+  Header, 
+  UserWrapper, 
+  UserInfo, 
+  Photo, 
+  User, 
+  UserGreeting, 
+  UserName, 
+  Icon, 
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList
+} from './styles';
+
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: { name: 'vendas', icon: 'dollar-sign' },
+      date: "13/04/2021",
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: "Desenvolvimento de site",
+      amount: "R$ 59,00",
+      category: { name: 'Alimentação', icon: 'coffee' },
+      date: "13/04/2021",
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: "Aluguel",
+      amount: "R$ 1.000,00",
+      category: { name: 'Casa', icon: 'shopping-bag' },
+      date: "13/04/2021",
+    }
+  ];
+
   return (
     <Container>
       <Header>
@@ -41,6 +87,17 @@ export function Dashboard() {
           type="total"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionsList 
+          data={data} 
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />} 
+        />
+
+      </Transactions>
     </Container>
   )
 }
