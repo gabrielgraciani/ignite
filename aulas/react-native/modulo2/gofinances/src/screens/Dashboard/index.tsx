6c7f65127085d2d7e91db68c78bb8c5eef748385
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-26 19:23:26 -0300

    creating card component

--- aulas/react-native/modulo2/gofinances/src/screens/Dashboard/index.tsx
+++ aulas/react-native/modulo2/gofinances/src/screens/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
-import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon } from './styles';
+import { Container, Header, UserWrapper, UserInfo, Photo, User, UserGreeting, UserName, Icon, HighlightCards } from './styles';
 
 export function Dashboard() {
   return (
@@ -19,6 +20,27 @@ export function Dashboard() {
           <Icon name="power" />
         </UserWrapper>
       </Header>
+
+      <HighlightCards>
+        <HighlightCard 
+          title="Entradas" 
+          amount="R$ 17.000,00" 
+          lastTransaction="última entrada dia 13 de abril" 
+          type="up"
+        />
+        <HighlightCard 
+          title="Saídas" 
+          amount="R$ 17.000,00" 
+          lastTransaction="última saída dia 13 de abril" 
+          type="down"
+        />
+        <HighlightCard 
+          title="Total" 
+          amount="R$ 00" 
+          lastTransaction="01 à 16 de abril" 
+          type="total"
+        />
+      </HighlightCards>
     </Container>
   )
 }
