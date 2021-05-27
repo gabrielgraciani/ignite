858ec3540e53a02a84ff5c2e4884b7568dd176af
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-05-27 20:01:29 -0300

    creating modal

--- aulas/react-native/modulo2/gofinances/src/screens/Register/index.tsx
+++ aulas/react-native/modulo2/gofinances/src/screens/Register/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { Modal } from 'react-native';
 
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../CategorySelect'
 
 import { 
   Container, 
@@ -16,9 +18,22 @@ import {
 
 export function Register() {
   const [transactionType, setTransactionType] = useState('');
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const [category, setCategory] = useState({
+    key: 'category',
+    name: 'Categoria',
+  });
 
   function handleTransactionsTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
+  }
+
+  function handleCloseSelectCategoryModal() {
+    setCategoryModalOpen(false);
+  }
+
+  function handleOpenSelectCategoryModal() {
+    setCategoryModalOpen(true);
   }
 
   return (
@@ -37,11 +52,15 @@ export function Register() {
             <TransactionTypeButton title="Outcome" type="down" isActive={transactionType === 'down'} onPress={() => handleTransactionsTypeSelect('down')} />
           </TransactionsTypes>
 
-          <CategorySelectButton title="Categoria" onPress={() => {}} />
+          <CategorySelectButton title={category.name} onPress={handleOpenSelectCategoryModal} />
         </Fields>
 
         <Button title="Enviar" onPress={() => {}} />
       </Form>
+
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect category={category} setCategory={setCategory} closeSelectCategory={handleCloseSelectCategoryModal} />
+      </Modal>
     </Container>
   )
 }
