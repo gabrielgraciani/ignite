79c8eaee4d34270757c4da1f56f7ed55a009a374
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-06-04 11:30:51 -0300

    creating card component

--- aulas/react-native/modulo3/rentx/src/screens/Home/index.tsx
+++ aulas/react-native/modulo3/rentx/src/screens/Home/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import { Container, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
+
+import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 export function Home(): JSX.Element {
+  const carData = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  };
+
   return (
     <Container>
-      <Title>oi</Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carData} />
+      <Car data={carData} />
     </Container>
   );
 }
