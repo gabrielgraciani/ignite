d68a0137619e4099d9d8c6b5888301272e1fc8e4
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-06-04 12:34:35 -0300

    creating car detail page

--- aulas/react-native/modulo3/rentx/src/screens/CarDetails/index.tsx
+++ aulas/react-native/modulo3/rentx/src/screens/CarDetails/index.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
+
 import { Button } from '../../components/Button';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
 
-import { Container, Header, CarImages } from './styles';
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+} from './styles';
 
 export function CarDetails(): JSX.Element {
   return (
     <Container>
       <Header>
-        <Button onPress={() => {}} />
+        <Button
+          onPress={() => {
+            console.log('teste');
+          }}
+        />
       </Header>
 
       <CarImages>
@@ -18,6 +45,36 @@ export function CarDetails(): JSX.Element {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Acessories>
+          <Acessory name="380Km/h" icon={speedSvg} />
+          <Acessory name="3.2s" icon={accelerationSvg} />
+          <Acessory name="800 HP" icon={forceSvg} />
+          <Acessory name="Gasolina" icon={gasolineSvg} />
+          <Acessory name="Auto" icon={exchangeSvg} />
+          <Acessory name="2 pessoas" icon={peopleSvg} />
+        </Acessories>
+
+        <About>
+          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+          ipsum lorem ipsum
+        </About>
+      </Content>
     </Container>
   );
 }
