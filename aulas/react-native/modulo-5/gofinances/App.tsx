184eebfd0f3c3bd8893b69daf7f617bb1261b0e3
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-06-30 21:16:47 -0300

    initial commit module 5

---
+++ aulas/react-native/modulo-5/gofinances/App.tsx
@@ -0,0 +1,44 @@
+import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { AuthProvider, useAuth } from './src/hooks/auth';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { Routes } from './src/routes'
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  const { userStorageLoading } = useAuth();
+
+  if (!fontsLoaded || userStorageLoading) {
+    return <AppLoading />
+  }
+  
+  return (
+
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle='light-content' />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+
+}
---
+++ aulas/react-native/modulo-5/gofinances/src/components/TransactionCard/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { categories } from '../../utils/categories';
+
+import {
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategoryName,
+  Date,
+} from './styles'
+
+export interface TransactionCardProps {
+  type: 'up' | 'down';
+  name: string;
+  amount: string;
+  category: String;
+  date: string;
+}
+
+interface Props {
+  data: TransactionCardProps;
+}
+
+export function TransactionCard({data}: Props) {
+
+  const category = categories.filter(
+    item => item.key == data.category
+  )[0];
+
+  return (
+    	<Container>
+        <Title>
+          {data.name}
+        </Title>
+        <Amount type={data.type}>
+          {data.type == 'down' && '- ' }
+          {data.amount}
+        </Amount>
+        <Footer>
+          <Category>
+            <Icon name={category.icon} />
+            <CategoryName>
+              {category.name}
+            </CategoryName>
+          </Category>
+          <Date>
+            {data.date}
+          </Date>
+        </Footer>
+
+      </Container>
+  )
+}
