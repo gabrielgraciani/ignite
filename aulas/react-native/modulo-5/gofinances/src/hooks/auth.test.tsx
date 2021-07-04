69cf0873fcafb7d32b8d678cc816355a0d9b50b9
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-07-04 15:28:23 -0300

    added custom mocks

--- aulas/react-native/modulo-5/gofinances/src/hooks/auth.test.tsx
+++ aulas/react-native/modulo-5/gofinances/src/hooks/auth.test.tsx
@@ -1,25 +1,24 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+import { mocked } from 'ts-jest/utils';
+import { logInAsync } from 'expo-google-app-auth';
 
 import { AuthProvider, useAuth } from './auth';
 
-jest.mock('expo-google-app-auth', () => {
-  return {
-    logInAsync: () => {
-      return {
-        type: 'success',
-        user: {
-          id: 'any_id',
-          email: 'gabriel@hotmail.com',
-          name: 'gabriel',
-          photo: 'any_photo.png',
-        }
-      }
-    }
-  }
-})
+jest.mock('expo-google-app-auth');
 
 describe('Auth hook', () => {
   it('should be able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(logInAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'success',
+      user: {
+        id: 'any_id',
+        email: 'gabriel@hotmail.com',
+        name: 'gabriel',
+        photo: 'any_photo.png',
+      }
+    });
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     });
@@ -27,5 +26,20 @@ describe('Auth hook', () => {
     await act(() => result.current.signInWithGoogle());
 
     expect(result.current.user.email).toBe('gabriel@hotmail.com');
+  });
+
+  it('user should not connect if cancel authentication with google', async () => {
+    const googleMocked = mocked(logInAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'cancel',
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty('id');
   });
 });
